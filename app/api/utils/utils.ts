4f936a1b4ccd530bc66562c6e89521507ffc43c4
Blayne Marjama <blayne.marjama@gmail.com>
2024-04-23 17:14:53 -0700

    fixed time until next game starts

--- app/api/utils/utils.ts
+++ app/api/utils/utils.ts
@@ -3,21 +3,21 @@ import { formatDistanceToNow } from "date-fns/formatDistanceToNow";
 // Rumbles start every hour on the hours
 export const timeUntilNextRumble = () => {
   const now = new Date();
-  const utcOffset = now.getTimezoneOffset(); // Offset in minutes between UTC and local time
-  const pstOffset = 480; // PST is UTC-8 hours, which is 480 minutes
-  const differenceFromPST = utcOffset + pstOffset; // Calculate total offset from PST
+  const utcOffset = now.getTimezoneOffset();
+  const pstOffset = 480; // PST is UTC-8 hours
+  const differenceFromPST = pstOffset - utcOffset;
 
-  // Start time is at noon PST every day
-  const startTime = new Date(now);
-  startTime.setMinutes(startTime.getMinutes() + differenceFromPST); // Convert local time to PST
-  startTime.setHours(12, 0, 0, 0); // Set time to noon PST
+  let startTime = new Date(now);
+  startTime.setMinutes(startTime.getMinutes() + differenceFromPST);
+  startTime.setHours(12, 0, 0, 0);
 
-  if (now.getTime() > startTime.getTime()) {
-    // If the current time in PST has already passed noon, set the next rumble to the following day
+  if (now >= startTime) {
     startTime.setDate(startTime.getDate() + 1);
+    startTime = new Date(startTime); // Recreate date object to reset time correctly
+    startTime.setMinutes(startTime.getMinutes() + differenceFromPST);
+    startTime.setHours(12, 0, 0, 0);
   }
 
-  // Calculate the difference in minutes and return
   return `Next Rumble ${
     formatDistanceToNow(startTime, { addSuffix: true, includeSeconds: true })
   }.`;
